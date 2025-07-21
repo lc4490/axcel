200541cdbf51163a33e16bb287a8e837ffe7af7a
Leo <lc4490@nyu.edu>
2025-07-21 14:43:19 -0400

    add visibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -383,6 +383,7 @@ export default function Home() {
                           WebkitBoxOrient: "vertical",
                         }}
                       >
+                        {isMobile ? "HI" : ""}
                         {item}
                       </Typography>
                     </Box>
