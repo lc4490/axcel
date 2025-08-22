94e4f3877cc09cf01af33548d16f3b4901382052
Leo <lc4490@nyu.edu>
2025-08-22 11:52:25 -0400

    bug fix for signup route for vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,9 +2,11 @@ import Database, { Database as DBType } from "better-sqlite3";
 import fs from "fs";
 import path from "path";
 
-const dataDir = path.join(process.cwd(), "data");
-if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir, { recursive: true });
-const dbPath = path.join(dataDir, "app.db");
+const isVercel = !!process.env.VERCEL;
+
+const dbPath = isVercel
+  ? "/tmp/app.db"
+  : path.join(process.cwd(), "data", "app.db");
 
 declare global {
   // Prevent multiple connections during dev hot-reload
@@ -13,6 +15,12 @@ declare global {
 }
 
 function init(): DBType {
+  // If running locally, make sure ./data exists
+  if (!isVercel) {
+    const dataDir = path.join(process.cwd(), "data");
+    if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir, { recursive: true });
+  }
+
   const db = new Database(dbPath);
   db.pragma("journal_mode = WAL");
   db.exec(`
